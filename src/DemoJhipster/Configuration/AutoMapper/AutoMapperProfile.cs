dc82df3ca3e3b1ac886993afe29bbe0b9ee7e98e
SukhrajSourceFuse <sukhraj.mohd@sourcefuse.com>
2023-03-14 12:01:19 +0530

    First commit

---
+++ src/DemoJhipster/Configuration/AutoMapper/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using System.Linq;
+using Jhipster.Domain.Entities;
+using Jhipster.Dto;
+
+
+namespace Jhipster.Configuration.AutoMapper;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+    }
+}
